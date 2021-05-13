997c9a3af18c014ab42fcf4f3692e5128c3ca79e
awadbilal <awadbilal99@gmail.com>
2021-05-14 02:18:50 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+
+const findMatching = (arr, drivers) => {
+                return arr.filter ((string) => {
+                                if(string === drivers || string === drivers.toLowerCase() || string === drivers.toUpperCase()) return string = drivers;
+                });
+};
+
+const fuzzyMatch = (arr, drivers) => {
+                return arr.filter ((string) => {
+                                for(let i = 0; i < drivers.length; i++){
+                                                if(string[i] === drivers[i]) return string = drivers;
+                                }
+                });
+};
+
+const matchName = (arr, drivers) => {
+                return arr.filter ((string) => string.name === drivers);
+};
