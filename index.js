9a2e5b7658eb685b6fae42db5a2beb54db20f339
Buse Şentürk <buse.senturk@outlook.com>
2021-05-15 14:44:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution here
+function findMatching (driversArr, string) {
+    return driversArr.filter((driver) => { return driver.toLowerCase()  ===  string.toLowerCase()})
+}
+
+function fuzzyMatch(driversArr, string){
+    return driversArr.filter((driver) => {
+        return driver[0] === string[0];
+    })
+}
+
+function matchName(driversArr, string){
+    return driversArr.filter((obj) => { return obj.name == string})
+}
